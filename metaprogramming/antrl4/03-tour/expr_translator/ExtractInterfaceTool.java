1c2ef6e940d727178a84dc510eb6bf5b4098e994
vinevtsev <vinevtsev@schleissheimer.de>
2021-09-07 08:28:52 +0200

    Misc formatting

--- metaprogramming/antrl4/03-tour/expr_translator/ExtractInterfaceTool.java
+++ metaprogramming/antrl4/03-tour/expr_translator/ExtractInterfaceTool.java
@@ -7,14 +7,22 @@ import org.antlr.v4.runtime.tree.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-public class ExtractInterfaceTool {
-    public static void main(String[] args) throws Exception {
+public class ExtractInterfaceTool
+{
+    public static void main(String[] args) throws Exception
+    {
         String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
+        if (args.length > 0)
+        {
+           inputFile = args[0];
+        }
+           
         InputStream is = System.in;
-        if ( inputFile!=null ) {
+        if (inputFile != null)
+        {
             is = new FileInputStream(inputFile);
         }
+        
         ANTLRInputStream input = new ANTLRInputStream(is);
 
         JavaLexer lexer = new JavaLexer(input);
