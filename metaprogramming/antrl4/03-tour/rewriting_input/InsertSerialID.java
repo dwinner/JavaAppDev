6444eb8d92f51bf87d0e2922955b5daa30a4ae9e
vinevtsev <vinevtsev@schleissheimer.de>
2021-09-07 10:11:59 +0200

    Trying out rewriting possibilities on the grammar walking via lexer

--- metaprogramming/antrl4/03-tour/rewriting_input/InsertSerialID.java
+++ metaprogramming/antrl4/03-tour/rewriting_input/InsertSerialID.java
@@ -1,11 +1,3 @@
-/***
- * Excerpted from "The Definitive ANTLR 4 Reference",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
-***/
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -16,13 +8,21 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 public class InsertSerialID {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
+        
+        if (args.length > 0)
+        {
+            inputFile = args[0];
+        }
+            
         InputStream is = System.in;
-        if ( inputFile!=null ) {
+        if (inputFile != null)
+        {
             is = new FileInputStream(inputFile);
         }
+        
         ANTLRInputStream input = new ANTLRInputStream(is);
 
         JavaLexer lexer = new JavaLexer(input);
